c08a7c30f48150fe3f5403010f3e42ab08421f75
mhmd7776 <mhmd7776@yahoo.com>
2022-08-17 00:03:08 +0430

    Add project files.

---
+++ KavenegarSample/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KavenegarSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> SendPublicSMS()
+        {
+            return Redirect("/");
+        }
+
+        public async Task<IActionResult> SendLookupSMS()
+        {
+            return Redirect("/");
+        }
+    }
+}
