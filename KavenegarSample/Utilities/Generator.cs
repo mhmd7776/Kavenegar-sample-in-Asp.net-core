c2ba788e759e6b3bee80ecd101772299875d3d5d
mhmd7776 <mhmd7776@yahoo.com>
2022-08-17 00:12:55 +0430

    Add Generator Class

---
+++ KavenegarSample/Utilities/Generator.cs
@@ -0,0 +1,14 @@
+﻿namespace KavenegarSample.Utilities
+{
+    public static class Generator
+    {
+        public static string RandomNumber()
+        {
+            var generator = new Random();
+
+            var result = generator.Next(0, 100000).ToString("D5");
+
+            return result;
+        }
+    }
+}
